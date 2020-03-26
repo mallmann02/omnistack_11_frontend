0e5f57149b93ba998230976c2b2b5c0ddd6e50bc
leonardo sm <leomallmann020302@gmail.com>
2020-03-26 16:57:10 -0300

    front-completion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
 export default App;
+
+/**
+ * Componente no react é uma função que retorna HTML
+ * JSX: é quando o html ta contido dentro do javascript
+ * Propriedades: atributos repassados para componentes
+ * 
+ * Componente tipo o "Header.js" tem que começar com letra maiúscula
+ * Propriedade no Header (title)
+ *    {props.title} e <Header title=""/>
+ *    {props.children} e <Header> Semana omnistack</Header>
+ * 
+ * Fazer botão chamar função:
+ *    Cria uma funcao pra ele dentro da função principal
+ *    no botão: onClick={<função>}
+ *  EXEMPLO de uma função que incrementar um contador:
+ *    fora da função: const [counter, setCounter] = useState(0)
+ *    dentro da funcao: setCounter(counter + 1)
+ *    
+ *   
+ * Usar estado para atualizar um componente
+ *    let <variavel qualquer> = useState
+ *        (0) => para numeros
+ *        [] => para arrays
+ *        ('') => para strings
+ * O useState retorna um array com o seguinte conteudo:
+ *    Array [valor, funçaoDeAtualizacao]
+ *    
+ */
